378260b221de0d28b527b92dd786da698aa0d91a
Francisco Trucco <franciscoctrucco@gmail.com>
2017-05-06 19:54:47 -0300

    Docstring for composition

--- discrete/composition.py
+++ discrete/composition.py
@@ -6,6 +6,8 @@ from discretes import random_bernoulli
 
 def composition(p, F):
     """Simulate variable X = sum([p[i]*F[i] for i in range(len(p))])
+    This method is applied when the distribution function can be expressed as a
+    linear combination of other distribution functions.
 
     :param p: List of weights. sum(p[i]) == 1
     :param F: List random number generators with distribution F[i]
