b762fe2dc0d2d2678605d4148b396f19f7c259b4
Francisco Trucco <franciscoctrucco@gmail.com>
2017-05-06 19:47:04 -0300

    Add composition

--- discrete/composition.py
+++ discrete/composition.py
@@ -1,15 +1,47 @@
 from random import random
+from discretes import uniform_discrete
+from discretes import poisson
+from discretes import random_bernoulli
 
-def compos(p, F):
-    """
-    El algoritmo es básicamente:
-    - Genero va Discreta I (1...n). Genera el valor j
-    - Calculo P(I = j) = alphaj
-    - Genero Fj (sé generarla)
-    - Ahora hago Fj * alphaj
-    - Repito hasta n
 
-    :param p: Arreglo de probabilidades de que I tome el valor i
-    :param F: Arreglo de simulaciones de funciones fi cuya distribucion es Fi
-              correspondientemente
+def composition(p, F):
+    """Simulate variable X = sum([p[i]*F[i] for i in range(len(p))])
+
+    :param p: List of weights. sum(p[i]) == 1
+    :param F: List random number generators with distribution F[i]
     """
+
+    U = random()
+    j = 0
+    acc = p[j]
+    while U >= acc:
+        j += 1
+        acc += p[j]
+
+    return F[j]()
+
+
+if __name__ == '__main__':
+    def mean(g, N=10000):
+        return sum([g() for _ in range(N)]) / N
+
+    p = [0.1, 0.2, 0.3, 0.4]
+
+    def F1(): return uniform_discrete(0, 10)
+
+    def F2(): return poisson(5)
+
+    def F3(): return random_bernoulli(0.3)
+
+    def F4(): return random_bernoulli(0.5)
+
+    F = [F1, F2, F3, F4]
+
+    print('\nComposition')
+    print(mean(lambda: composition(p, F)))
+
+    def random_F():
+        return sum([p[i] * F[i]() for i in range(len(p))])
+
+    print('\nNo Composition')
+    print(mean(lambda: random_F()))
