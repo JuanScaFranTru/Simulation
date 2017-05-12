3ff17e5c8e8b44b91668b63fbcfd5ce05de27da4
juan_sca <juansca1229@gmail.com>
2017-05-12 13:37:31 -0300

    Implement hom_poisson_alternative. Add some comments

--- continuous/poisson.py
+++ continuous/poisson.py
@@ -1,5 +1,6 @@
 from random import random
 from math import log
+from discrete.discretes import poisson_optimized
 
 
 def homogeneous_poisson_events(T, lam):
@@ -9,17 +10,22 @@ def homogeneous_poisson_events(T, lam):
     número de eventos que ocurren hasta el tiempo T , S[I] indica el tiempo en
     que ocurre cada evento, 1 ≤ I ≤ n (n será el máximo valor alcanzado por I)
     """
+    # Es el momento 0
     t = 0
+    # Ocurrieron 0 eventos
     I = 0
     S = [0] * 10
-    S[0] = 0
     while True:
         U = random()
-        if t - log(U) / lam:
+        # Si ya pasaron las T unidades de tiempo, terminamos
+        if t - log(U) / lam > T:
             break
+        # Si no, avanzamos en el tiempo
         else:
             t -= log(U) / lam
+        # Ocurre otro evento
         I += 1
+        # En el momento t
         S[I] = t
 
 
@@ -28,7 +34,14 @@ def hom_poisson_alternative(T, lam):
     Dado N (T), la distribución de los tiempos de arribo en un
     Proceso de Poisson homogéneo de razón λ es uniforme en (0, T)
     """
-    pass
+    # Número de arribos hasta el tiempo T
+    n = poisson_optimized(lam * T)
+    # Dado N(T) la distribucion de los tiempos de arribo es uniforme en (0,T)
+    uniforms = [random(0, T) for _ in range(n)]
+    uniforms.sort()
+    arrive_times = [T * U for U in uniforms]
+
+    return arrive_times
 
 
 def non_homogeneous_poisson(lam):
