bd0d07ec3f6ef22607102ccf3b7f290df348fb23
juan_sca <juansca1229@gmail.com>
2017-06-04 14:25:11 -0300

    Optimize ej2

--- Practico6/ej2.py
+++ Practico6/ej2.py
@@ -6,14 +6,19 @@ def mean_estimation(min_simlation, accept_value):
     mean = 0
     var = 0
     data_len = 0
-    while data_len < min_simlation or (var / data_len) ** (1/2) > accept_value:
+    while data_len < min_simlation or var > accept_value:
+        # Or (is much better the aproximation, tradeoff with time)
+        # while data_len < min_simlation or (var / data_len) > accept_value:
         data_len += 1
         X = exp(random() ** 2)
         old_mean = mean
-        mean += (1 / data_len) * (X - mean)
+        mean += (X - mean) / data_len
         if var != 0:
             var *= 1 - 1 / (data_len - 1)
-        var += data_len * (mean - old_mean)**2
+        var += data_len * (mean - old_mean)
+        # Or (is much better the aproximation, tradeoff with time)
+        # var += float(data_len * (mean - old_mean)) ** 2
+
     return (mean, var, data_len)
 
 
